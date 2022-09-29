959fe1da603e8aed09618eef0d1ce3d771cabd5e
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-29 09:13:36 -0400

    Added remaining labels and buttons to interface and changed colors to match the theme.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 This application is for timing your work/learning intervals.
 
-It utilizes the following imported modules: `tkinter` """
+It utilizes the following imported modules: `tkinter` and 'time' """
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -27,6 +27,7 @@ window = Tk()
 window.title("Pomodoro")
 # Configure window by adding padding to provide more space around image.
 window.config(padx=100, pady=50, bg=YELLOW)
+
 # Create canvas for window.
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 # Create PhotoImage in order to use image on canvas.
@@ -34,7 +35,19 @@ tomato_image = PhotoImage(file="tomato.png")
 # Adds tomoato image to canvas and displays it on the window.
 canvas.create_image(100, 112, image=tomato_image)
 canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# Create labels for timer and checkmarks.
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+timer_label.grid(column=1, row=0)
+checkmark = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+checkmark.grid(column=1, row=3)
+
+# Create buttons for Start and Reset.
+start = Button(text="Start", highlightbackground=YELLOW, fg=GREEN)
+start.grid(column=0, row=2)
+reset = Button(text="Reset", highlightbackground=YELLOW, fg=GREEN)
+reset.grid(column=2, row=2)
 
 
 
