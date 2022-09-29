9a37314db13e824facbcd64ed262a036de630b39
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-29 09:01:39 -0400

    Changed the background color of window and canvas and adjusted image location to ensure entire image is displayed.

--- main.py
+++ main.py
@@ -25,13 +25,15 @@ LONG_BREAK_MIN = 20
 # Create window.
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+# Configure window by adding padding to provide more space around image.
+window.config(padx=100, pady=50, bg=YELLOW)
 # Create canvas for window.
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 # Create PhotoImage in order to use image on canvas.
 tomato_image = PhotoImage(file="tomato.png")
 # Adds tomoato image to canvas and displays it on the window.
-canvas.create_image(103, 112, image=tomato_image)
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 
