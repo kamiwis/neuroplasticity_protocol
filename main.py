1cd62f121476d185066c0cdeab535fdf3c299d2e
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-29 09:55:44 -0400

    Added countdown mechanism that is linked to the Start button being pressed.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ This application is for timing your work/learning intervals.
 
 It utilizes the following imported modules: `tkinter` and 'time' """
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,15 +12,52 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECKMARK = "✓"
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_s = SHORT_BREAK_MIN * 60
+    long_break_s = LONG_BREAK_MIN * 60
+    if reps == 8:
+        timer_label.config(text="Break", fg=RED)
+        countdown(long_break_s)
+    if reps % 2 == 0:
+        timer_label.config(text="Break", fg=RED)
+        countdown(short_break_s)
+    else:
+        timer_label.config(text="Work")
+        countdown(work_sec)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# Define countdown mechanism.
+def countdown(count):
+    """Takes a time as the argument and countsdown from that time to 0."""
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec == 0:
+        count_sec = "00"
+    if count < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+    if count == 0:
+        start_timer()
+        num_work_sessions = reps // 2
+        mark = ""
+        for _ in range(num_work_sessions):
+            mark += CHECKMARK
+        checkmark.config(text=f"{mark}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Create window.
@@ -34,22 +72,19 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 # Adds tomoato image to canvas and displays it on the window.
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # Create labels for timer and checkmarks.
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 timer_label.grid(column=1, row=0)
-checkmark = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+checkmark = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
 checkmark.grid(column=1, row=3)
 
 # Create buttons for Start and Reset.
-start = Button(text="Start", highlightbackground=YELLOW, fg=GREEN)
+start = Button(text="Start", highlightbackground=YELLOW, fg=GREEN, command=start_timer)
 start.grid(column=0, row=2)
 reset = Button(text="Reset", highlightbackground=YELLOW, fg=GREEN)
 reset.grid(column=2, row=2)
-
-
-
 
 window.mainloop()
